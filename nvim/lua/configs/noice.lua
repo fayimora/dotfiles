7b1aaffe34ff2d9f9410c4b1bd510cec8cf20cf7
Fayi <fayi@fayimora.com>
2024-06-04 18:27:38 +0100

    Noice fixes

--- nvim/lua/configs/noice.lua
+++ nvim/lua/configs/noice.lua
@@ -1,7 +1,20 @@
 local M = {}
 
 M.setup = function()
-  require("noice").setup({
+  require("noice").setup {
+    lsp = {
+      override = {
+        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+        ["vim.lsp.util.stylize_markdown"] = true,
+        ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+      },
+      hover = {
+        enabled = false,
+      },
+      signature = {
+        enabled = false,
+      },
+    },
     routes = {
       {
         filter = {
@@ -11,8 +24,14 @@ M.setup = function()
         },
         opts = { skip = true },
       },
+
+      -- always route any messages with more than 20 lines to the split view
+      {
+        view = "split",
+        filter = { event = "msg_show", min_height = 20 },
+      },
     },
-  })
+  }
 end
 
 return M
