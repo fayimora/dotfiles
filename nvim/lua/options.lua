6d63ea16744a93b07a743af6e5f8b7f2d8750143
Fayi <fayi@fayimora.com>
2024-08-16 22:25:43 +0100

    basic vim settings

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -2,5 +2,9 @@ require "nvchad.options"
 
 -- add yours here!
 
--- local o = vim.o
+local opt = vim.opt
 -- o.cursorlineopt ='both' -- to enable cursorline!
+--
+opt.relativenumber = true
+opt.number = true
+opt.backspace = "indent,eol,start"
