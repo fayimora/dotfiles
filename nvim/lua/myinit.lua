b713b08a9e1b183e13a0ec285797983901b5ea9a
Fayi <fayi@fayimora.com>
2024-09-06 12:04:01 +0100

    ft for .env and .envrc files

--- nvim/lua/myinit.lua
+++ nvim/lua/myinit.lua
@@ -12,12 +12,6 @@ vim.api.nvim_create_autocmd(
   { group = hocon_group, pattern = "*/resources/*.conf", command = "set ft=hocon" }
 )
 
-vim.filetype.add {
-  extension = {
-    ["http"] = "http",
-  },
-}
-
 vim.opt_global.shortmess:remove "F"
 
 vim.diagnostic.config {
@@ -25,3 +19,29 @@ vim.diagnostic.config {
   update_in_insert = true,
   virtual_text = false,
 }
+
+vim.filetype.add {
+  -- Detect and assign filetype based on the extension of the filename
+  extension = {
+    mdx = "mdx",
+    log = "log",
+    conf = "conf",
+    env = "dotenv",
+    http = "http",
+  },
+  -- Detect and apply filetypes based on the entire filename
+  filename = {
+    [".env"] = "dotenv",
+    ["env"] = "dotenv",
+    ["tsconfig.json"] = "jsonc",
+  },
+  -- Detect and apply filetypes based on certain patterns of the filenames
+  pattern = {
+    -- match filenames like .envrc, .envrc.private
+    ["%.envrc%.[%w_.-]+"] = "sh",
+    -- INFO: Match filenames like - ".env.example", ".env.local" and so on
+    ["%.env%.[%w_.-]+"] = "dotenv",
+  },
+}
+
+-- vim.api.nvim_set_hl(0, "CmpItemKindSupermaven", { fg = "#6CC644" })
