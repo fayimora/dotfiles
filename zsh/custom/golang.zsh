0c57c472fba1c91a03d1b576521c4b4b05db8268
Fayi Femi-Balogun <fayi@fayimora.com>
2025-08-16 22:47:42 +0100

    Fix go paths.. macos vs linux

--- zsh/custom/golang.zsh
+++ zsh/custom/golang.zsh
@@ -1,2 +1,14 @@
-export GOPATH=$HOME/go 
-export PATH=$PATH:/usr/local/go/bin:$GOPATH/bin
+case "$(uname)" in
+    Darwin)
+        OS_TYPE="Mac"
+        export GOPATH=$HOME/go
+        export PATH=$PATH:/usr/local/go/bin:$GOPATH/bin
+        ;;
+    Linux)
+        export GOPATH=$(mise which go)
+        ;;
+    *)
+        echo "Unsupported OS while setting GOPATH"
+        exit 1
+        ;;
+esac
