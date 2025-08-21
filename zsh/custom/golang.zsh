d915c4713826b2371c11d56cc60b29e566eebc90
Fayi Femi-Balogun <fayi@fayimora.com>
2025-08-21 21:25:54 +0100

    update golang zsh config

--- zsh/custom/golang.zsh
+++ zsh/custom/golang.zsh
@@ -1,14 +1,15 @@
-case "$(uname)" in
-    Darwin)
-        OS_TYPE="Mac"
-        export GOPATH=$HOME/go
-        export PATH=$PATH:/usr/local/go/bin:$GOPATH/bin
-        ;;
-    Linux)
-        export GOPATH=$(mise which go)
-        ;;
-    *)
-        echo "Unsupported OS while setting GOPATH"
-        exit 1
-        ;;
-esac
+export GOPATH=$HOME/go
+# case "$(uname)" in
+#     Darwin)
+#         OS_TYPE="Mac"
+#         export GOPATH=$HOME/go
+#         export PATH=$PATH:/usr/local/go/bin:$GOPATH/bin
+#         ;;
+#     Linux)
+#         # export GOPATH=$(mise which go)
+#         ;;
+#     *)
+#         echo "Unsupported OS while setting GOPATH"
+#         exit 1
+#         ;;
+# esac
