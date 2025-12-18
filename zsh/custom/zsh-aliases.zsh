3612303218b818bbb683713d9be3170ae235ef92
Fayi Femi-Balogun <fayi@fayimora.com>
2025-12-18 15:03:31 +0000

    Add better nvim alias

--- zsh/custom/zsh-aliases.zsh
+++ zsh/custom/zsh-aliases.zsh
@@ -16,3 +16,23 @@ alias -g .....='../../../..'
 # to find all files containing 'foo' in the name
 function fn() { ls **/*$1* }
 
+# Initial solution from https://johnnymatthews.dev/blog/2025-10-02-open-neovim-at-specified-directory/
+# but extended it to work for files
+nvim() {
+  if [ $# -eq 1 ]; then
+    if [ -d "$1" ]; then
+      # Argument is a directory - cd into it
+      command nvim -c "cd $1"
+    elif [ -f "$1" ]; then
+      # Argument is a file - cd into its parent directory and open the file
+      local dir
+      dir=$(dirname "$1")
+      command nvim -c "cd $dir" "$1"
+    else
+      # File/dir doesn't exist yet, just pass through
+      command nvim "$@"
+    fi
+  else
+    command nvim "$@"
+  fi
+}
